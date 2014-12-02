8b108a9a5a6a8029b5daa157b7d516583220fd76
Takuya Kato <github@takuyan.com>
2014-12-02 14:38:47 +0900

    add home-page

---
+++ config.rb
@@ -0,0 +1,94 @@
+require 'dotenv'
+Dotenv.load
+require 'bootstrap-sass'
+
+Time.zone = 'Tokyo'
+
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+###
+# AWS S3 Setting
+###
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'www.takuyan.com'
+  s3_sync.region                     = 'ap-northeast-1'
+  s3_sync.aws_access_key_id          = ENV['S3_ACCESS']
+  s3_sync.aws_secret_access_key      = ENV['S3_SECRET']
+  #s3_sync.delete                     = false
+  #s3_sync.after_build                = false
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+end
